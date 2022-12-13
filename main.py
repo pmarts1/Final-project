998ebf7e60b433c2793de60184b0d65905f9b699
developer1 <martsinovskii.pa@phystech.edu>
2022-12-13 14:51:44 +0300

    a

--- main.py
+++ main.py
@@ -1,24 +1,152 @@
 import pygame
 from objects import figure
 from objects import game_field
-FPS = 3
-screenWidth = 1920
-screenHeight = 1080
+import copy
+from objects import screen
+
+
 gameFieldHeight = 20
 gameFieldWidth = 10
 BLACK = 0x000000
-
+FPS = 60
 pygame.init()
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+
 
 
 clock = pygame.time.Clock()
 
 finished = False
+level_table = [48, 43, 38, 33, 28, 23, 18, 13, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+
+#field1 = game_field(100, 100, 400, 10, 10)
+frame = 0
+level = 5
+finished = False
+moving_left_start = 0
+moving_left = 0
+moving_right_start = 0
+moving_right = 0
+moving_down_start = 0
+def run_game(figure, field):
+    global moving_down_start
+    global moving_left
+    global moving_left_start
+    global moving_right
+    global moving_right_start
+    global finished
+    global frame
+    global level
+    frame += 1
+    clock.tick(FPS)
+
+    if frame - moving_down_start == level_table[level]:
+        yold = figure.y
+        figure.move_down(field)
+        if figure.y != yold:
+            moving_down_start = frame
 
 
-while not finished:
+    if frame - moving_down_start == level_table[level] + 30:
+        yold = figure.y
+        figure.move_down(field)
+        if figure.y != yold:
+            moving_down_start = frame
+        else:
+            field.update_static_field(figure)
+            figure.new_figure()
+            moving_left_start = 0
+            moving_left = 0
+            moving_right_start = 0
+            moving_right = 0
+            moving_down_start = frame
+            field.burn_filled_rows()
+
+    if moving_left == 1 and frame - moving_left_start == 16:
+        figure.move_left(field)
+        moving_left = 2
+        moving_left_start = frame
+    if moving_left == 2 and frame - moving_left_start == 6:
+        figure.move_left(field)
+        moving_left_start = frame
+
+    if moving_right == 1 and frame - moving_right_start == 16:
+        figure.move_right(field)
+        moving_right = 2
+        moving_right_start = frame
+    if moving_right == 2 and frame - moving_right_start == 6:
+        figure.move_right(field)
+        moving_right_start = frame
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                figure.move_left(field)
+                moving_left_start = frame
+                moving_left = 1
+            if event.key == pygame.K_d:
+                figure.move_right(field)
+                moving_right_start = frame
+                moving_right = 1
+            if event.key == pygame.K_s:
+                figure.move_down(field)
+                moving_down_start = frame
+            if event.key == pygame.K_q:
+                figure.rotate_counterclockwise(field)
+            if event.key == pygame.K_e:
+                figure.rotate_clockwise(field)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = 0
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                moving_right = 0
+
+        #print(moving_down_start - frame)
+    #print(pygame.event.get())
+
+
+
+'''while not finished:
+    screen.fill(BLACK)
+    g1.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                gun.fire2_end(event)
+            elif event.type == pygame.MOUSEMOTION:
+                gun.targetting(event)
+
     '''
-    Основной цикл программы.
-    '''
+    #Основной цикл программы.
+'''
+    clock.tick(1)
 pygame.quit()
+'''
+f1 = figure()
+f1.new_figure()
+g1 = game_field(100, 100, 400, 100, 100)
+#oldField = g1.static_field
+print(g1.game_field_height)
+while not finished:
+    screen.fill(BLACK)
+
+    g1.draw()
+    pygame.display.update()
+    run_game(f1, g1)
+    g1.update_field_for_drawing(f1)
+
+    #print(g1.field)
+    #if oldField != g1.static_field:
+    #print(g1.field)
+    #oldField = g1.static_field
+    #clock.tick(1)
+    #print(pygame.event.get())
+    #for i in pygame.event.get():
+    #print(pygame.event.get())
--- objects.py
+++ objects.py
@@ -12,7 +12,13 @@ RED = 0xFF0000
 BLUE = 0x0000FF
 YELLOW = 0xFFC91F
 GREEN = 0x00FF00
-GAME_COLORS = [RED, BLUE, YELLOW, GREEN]
+BLACK = 0x000000
+GAME_COLORS = [BLACK, RED, BLUE, YELLOW, GREEN]
+
+screenWidth = 1000
+screenHeight = 1000
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+
 '''
 Цвета, случайно выбирающиеся при появлении новой фигуры
 '''
@@ -64,7 +70,6 @@ tetromino_list = [tT, jT, lT, sT, zT, iT, oT]
 '''
 
 
-
 class figure:
     '''
     Фигуры, состоящие из 4-х квадратиков.
@@ -76,7 +81,7 @@ class figure:
         '''
         Координаты цетра вращения фигуры...
         '''
-        self.color = RED
+        self.color = 1
         self.coordinates = [[0, 0, 0, 0, 0],
                             [0, 0, 0, 0, 0],
                             [0, 0, 0, 0, 0],
@@ -90,26 +95,31 @@ class figure:
         '''
         Список, описывающий текущий поворот фигуры. 0 - пустая клетка, 1 - заполненная
         '''
+
     def new_figure(self):
         self.x = 5
         self.y = 0
         self.coordinates = choice(tetromino_list)
-        self.color = choice(GAME_COLORS)
+        self.color = random.randint(1, 4)
         '''
         Создает новый тетрамино сверху экрана со случайным выбором новой формы и цвета
         '''
+
     def check_movement_possibility(self, field):
         for i in range(5):
             for j in range(5):
-                if self.supporting_coordinates[i][j] == 1 and (self.x + j - 2 > 9 or self.x + j - 2 < 0 or self.y + i - 2 > 19 or field.static_field[self.y + i - 2][self.x + j - 2] != 0):
+                if self.supporting_coordinates[i][j] == 1 and (
+                        self.x + j - 2 > 9 or self.x + j - 2 < 0 or self.y + i - 2 > 19 or
+                        field.static_field[self.y + i - 2][self.x + j - 2] != 0):
                     return False
         return True
+
     def move_down(self, field):
         '''
         Движение вниз на одну клетку.
         '''
         self.supporting_coordinates = self.coordinates
-        self.y +=1
+        self.y += 1
         if self.check_movement_possibility(field) == False:
             self.y -= 1
         self.supporting_coordinates = [[0, 0, 0, 0, 0],
@@ -166,7 +176,7 @@ class figure:
         for i in range(5):
             for j in range(5):
                 if self.coordinates[i][j] == 1:
-                    self.supporting_coordinates[4-j][i] = 1
+                    self.supporting_coordinates[4 - j][i] = 1
         if self.check_movement_possibility(field) == True:
             self.coordinates = self.supporting_coordinates
             self.supporting_coordinates = [[0, 0, 0, 0, 0],
@@ -174,6 +184,7 @@ class figure:
                                            [0, 0, 0, 0, 0],
                                            [0, 0, 0, 0, 0],
                                            [0, 0, 0, 0, 0]]
+
 
 class game_field():
     '''
@@ -189,8 +200,8 @@ class game_field():
         """
         self.x0 = x0
         self.y0 = y0
-        self.game_field_width = game_field_width
-        self.game_field_height = game_field_height *2
+        self.game_field_width = copy.deepcopy(game_field_width)
+        self.game_field_height = copy.deepcopy(game_field_width) * 2
         self.info_field_width = info_field_width
         self.info_field_height = info_field_height
 
@@ -202,17 +213,21 @@ class game_field():
         """
         Cписок клеток. 0 - пустая клетка. Другая цифра (в зависимости от цвета) - заполненная клетка.
         """
+
     def update_field_for_drawing(self, moving_figure):
         self.field = copy.deepcopy(self.static_field)
         for i in range(5):
             for j in range(5):
-                if moving_figure.y + i - 2 < 19 and moving_figure.x + j - 2 < 10:
-                    self.field[moving_figure.y + i - 2][moving_figure.x + j - 2] = moving_figure.coordinates[i][j]
+                if moving_figure.y + i - 2 < 20 and moving_figure.x + j - 2 < 10 and moving_figure.coordinates[i][j] != 0:
+                    self.field[moving_figure.y + i - 2][moving_figure.x + j - 2] = moving_figure.coordinates[i][j] * moving_figure.color
+
     def draw(self):
-        '''
-        Рисует ячейки из field (движущаяся фигура должна быть уже отображена в field)
-        '''
-        pass
+        for i in range(20):
+            for j in range(10):
+                pygame.draw.rect(screen, GAME_COLORS[self.field[i][j]],
+                                 pygame.Rect(self.x0 + self.game_field_width / 10 * j,
+                                             self.y0 + self.game_field_height / 20 * i, self.game_field_width / 10,
+                                             self.game_field_height / 20))
 
     def burn_filled_rows(self):
         rows_to_burn = []
@@ -224,13 +239,17 @@ class game_field():
             for j in range(10):
                 if self.field[i][j] == 0:
                     row_fullness_check = False
-                continue
+                #continue
             if row_fullness_check == True:
                 rows_to_burn.append(i)
-                for j in range(10):
-                    self.field[i][j] = 5
+                '''for j in range(10):
+                    self.field[i][j] = 5'''
+            row_fullness_check = True
+        if len(rows_to_burn) != 0:
+            for i in range(rows_to_burn[0]):
+                self.field[rows_to_burn[len(rows_to_burn) - 1] - i] = copy.deepcopy(self.field[rows_to_burn[len(rows_to_burn) - 1] - len(rows_to_burn) - i])
+            self.static_field = copy.deepcopy(self.field)
 
-
-
-
-
+    def update_static_field(self, moving_figure):
+        self.update_field_for_drawing(moving_figure)
+        self.static_field = copy.deepcopy(self.field)
