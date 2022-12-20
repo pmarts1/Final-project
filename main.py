9b5b14a06a0e3d24772f8356564f861104300892
developer1 <martsinovskii.pa@phystech.edu>
2022-12-20 13:53:14 +0300

    a

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ from objects import screen
 gameFieldHeight = 20
 gameFieldWidth = 10
 BLACK = 0x000000
+WHITE = (255, 255, 255)
+GREY = 0xc0c0c0
 FPS = 30
 pygame.init()
 
 a = []
 
 clock = pygame.time.Clock()
-
+fnt = pygame.font.Font(None, 72)
 finished = False
 level_table = [48, 43, 38, 33, 28, 23, 18, 13, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
 
@@ -181,6 +183,46 @@ def enter_name():
                     name += event.unicode
         print(name)
     return name
+
+def menu():
+    start = False
+    mode = 1
+    global fnt
+    while not start:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN and mode < 4:
+                    mode = mode + 1
+                if event.key == pygame.K_UP and mode > 1:
+                    mode = mode - 1
+                if event.key == pygame.K_RETURN:
+                    start = True
+
+        if mode == 1:
+            player = fnt.render("1 PLAYER", 1, GREY)
+        else:
+            player = fnt.render("1 PLAYER", 1, WHITE)
+        screen.blit(player, (800, 100))
+        if mode == 2:
+            player2 = fnt.render("P V P", 1, GREY)
+        else:
+            player2 = fnt.render("P V P", 1, WHITE)
+        screen.blit(player2, (800, 300))
+        if mode == 3:
+            recs = fnt.render("RECORDS", 1, GREY)
+        else:
+            recs = fnt.render("RECORDS", 1, WHITE)
+        screen.blit(recs, (800, 500))
+        if mode == 4:
+            q = fnt.render("QUIT", 1, GREY)
+        else:
+            q = fnt.render("QUIT", 1, WHITE)
+        screen.blit(q, (800, 700))
+        pygame.display.update()
+        clock.tick(FPS)
+    return mode
+    print('ffffff')
+
 '''while not finished:
     screen.fill(BLACK)
     g1.draw()
@@ -229,11 +271,9 @@ lowest = records[9][0]
 while not finished:
     if option == 0:
         print('bbbbbbbbb')
-        #menu()
-        screen.fill((255, 255, 255))
-        pygame.display.update()
-        clock.tick(0.01)
-    #pass
+        screen.fill((BLACK))
+        option = menu()
+
     if option == 1:
         frame = 0
         gameOver = False
@@ -268,11 +308,15 @@ while not finished:
             a = pygame.event.get()
             if g1.game_over == False:
                 run_game(f1, g1, pygame.K_a, pygame.K_d, pygame.K_s, pygame.K_e, pygame.K_q)
-            #else:
+            else:
+                score1 = fnt.render(str(g1.score), 1, WHITE)
+                screen.blit(score1, (500, 600))
 
             if g2.game_over == False:
                 run_game(f2, g2, pygame.K_j, pygame.K_l, pygame.K_k, pygame.K_o, pygame.K_u)
-            #else:
+            else:
+                score2 = fnt.render(str(g2.score), 1, WHITE)
+                screen.blit(score2, (1500, 600))
             if g1.game_over and g2.game_over:
                 #clock.tick(1)
                 #gameOver = True
@@ -281,7 +325,6 @@ while not finished:
                         gameOver = True
             pygame.display.update()
         option = 0
-
 
     if option == 4:
         finished = True
